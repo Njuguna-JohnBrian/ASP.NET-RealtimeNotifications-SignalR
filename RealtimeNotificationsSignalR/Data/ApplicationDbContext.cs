30fbbe1f92716b1b3b795cbd41aad227fb9431b8
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-06 02:21:46 +0300

    create db context

--- RealtimeNotificationsSignalR/Data/ApplicationDbContext.cs
+++ RealtimeNotificationsSignalR/Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RealtimeNotificationsSignalR.Models;
 
 namespace RealtimeNotificationsSignalR.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
     }
 }
