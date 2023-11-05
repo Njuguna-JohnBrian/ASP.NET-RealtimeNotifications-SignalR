2f614cbf8a522083d7941f7de4c8b07a48966e08
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-06 01:29:51 +0300

    add product model

--- RealtimeNotificationsSignalR/Models/Product.cs
+++ RealtimeNotificationsSignalR/Models/Product.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RealtimeNotificationsSignalR.Models;
 
 public class Product
 {
-    
+    [Key] public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public decimal Price { get; set; } = decimal.Zero;
 }
