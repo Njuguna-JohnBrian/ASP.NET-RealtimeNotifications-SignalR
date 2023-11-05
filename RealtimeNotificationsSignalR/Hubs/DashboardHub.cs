ee015d397dc84b163a8aa52f3e2a53184a11bb05
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-06 02:47:01 +0300

    create signalr notifications hub

--- RealtimeNotificationsSignalR/Hubs/DashboardHub.cs
+++ RealtimeNotificationsSignalR/Hubs/DashboardHub.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.SignalR;
+using RealtimeNotificationsSignalR.Data;
+using RealtimeNotificationsSignalR.Repositories;
 
 namespace RealtimeNotificationsSignalR.Hubs;
 
-public class DashboardHub:Hub
+public class DashboardHub : Hub
 {
-    
+    private ProductRepository productRepository;
+    private readonly ApplicationDbContext _dbContext;
+
+    public DashboardHub(IConfiguration configuration, ApplicationDbContext dbContext)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        _dbContext = dbContext;
+        productRepository = new ProductRepository(connectionString, _dbContext);
+    }
+
+    public async Task SendProducts()
+    {
+        var products = productRepository.GetProducts();
+        await Clients.All.SendAsync("ReceivedProducts", products);
+    }
 }
