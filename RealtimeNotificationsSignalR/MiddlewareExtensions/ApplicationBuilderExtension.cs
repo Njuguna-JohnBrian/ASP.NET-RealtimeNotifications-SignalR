ec86c1a26e16e671d8637b0421d1094b2f142151
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-06 04:04:59 +0300

    create app builder extention

---
+++ RealtimeNotificationsSignalR/MiddlewareExtensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,14 @@
+using RealtimeNotificationsSignalR.SubscribeTableDependencies;
+
+namespace RealtimeNotificationsSignalR.MiddlewareExtensions;
+
+public static class ApplicationBuilderExtension
+{
+    public static void UseSqlTableDependency<T>(this IApplicationBuilder applicationBuilder, string connectionString)
+        where T : ISubscribeTableDependency
+    {
+        var serviceProvider = applicationBuilder.ApplicationServices;
+        var service = serviceProvider.GetService<T>();
+        service?.SubscribeTableDependency(connectionString);
+    }
+}
